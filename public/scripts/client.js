10f3d647f883789c2ec972d99440e06d31fba1e7
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-03-30 22:29:06 -0400

    Adj results display

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,25 +1,39 @@
 
 $(document).ready(function() {
   // console.log("DOC READY")
+  const clearOldResults = function() {
+    $('#expression').text("Expression:");
+    $('#result').text("Result:");
+  }
+
   const clickSubmit = function() {
+
     $('form button').on('click', event => {
       event.preventDefault();
+      clearOldResults();
       const leftOperand = $('#leftOperand').val()
       const rightOperand = $('#rightOperand').val()
       const operator = $("input[name='operator']:checked").val()
+
+      // const expression = leftOperand + operator + rightOperand
+      const expression = "3+3"
+      const result = "6"
+      $('#expression').text(`Expression: ${expression}`);
+      $('#result').text(`Result: ${result}`);
+
       $.ajax({
         method: 'POST',
         url: `http://localhost:8080/leftOperand=${leftOperand}rightOperand=${rightOperand}operation=${operator}`,
       })
         .then(res => {
+          console.log("THEN...")
           // const expression = res.data.Expression;
           // const result = res.data.Result;
-          console.log("THEN...")
-          const expression = "3+3"
-          const result = "6"
-          $('#expression').after(`<p>${expression}</p>`)
-          $('#result').after(`<p>${result}</p>`)
-        });
+          // $('#expression').text(`Expression: ${expression}`);
+          // $('#result').text(`Result: ${result}`)
+
+        })
+        .catch(e => console.log("ERROR:", e));
     })
   }
   clickSubmit();
