f7c41c881822cc84c5e55ab6ddf716a9fc90bc2f
Charles Prystupa <Charles.Prystupa@gmail.com>
2021-03-30 21:14:07 -0400

    Front End Posts query

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,18 +1,18 @@
 
 $(document).ready(function() {
-  initializePage();
-
-  fetchAndUpdateAll();
-
-  postTweetClick()
-
-  toggleTweetInputClick();
-
-  rtnToTopTrigger();
-
-  rtnToTopClick();
-
-  closeTweetInputScroll();
-
-
+  // console.log("DOC READY")
+  const clickSubmit = function() {
+    $('form button').on('click', event => {
+      event.preventDefault();
+      const leftOperand = $('#leftOperand').val()
+      const rightOperand = $('#rightOperand').val()
+      const operator = $("input[name='operator']:checked").val()
+      $.ajax({
+        method: 'POST',
+        url: `http://localhost:8080/leftOperand=${leftOperand}&rightOperand=${rightOperand}&operation=${operator}`,
+      })
+        .then(res => res);
+    })
+  }
+  clickSubmit();
 });
--- public/scripts/helpers.js
+++
@@ -1,4 +0,0 @@
-const createQuery = (leftOperand, rightOperand, operator) => {
-  let url = `http://localhost:8080/leftOperand=${leftOperand}&rightOperand=${rightOperand}&operation=${operator}`;
-  return url;
-}
